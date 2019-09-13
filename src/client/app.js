74b963c04f94205847e2bca1138da2335f0290f3
DoubleG86 <gaetanw@hotmail.com>
2019-09-13 09:45:34 +0200

    server install

--- src/client/app.js
+++ src/client/app.js
@@ -2,13 +2,34 @@
  *
  * /src/client/app.js - Client entry point
  *
- * coded by leny@BeCode
+ * coded by BackstreetBoys@BeCode
  * started at 06/09/2019
  */
 
-import * as React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import TypoGraphy from "@material-ui/core/Typography";
+import mainLogo from "../../src/logo.png";
+import NavBar from "../navbar";
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar color={"primary"} position={"static"}>
+                    <Toolbar>
+                        <img src={mainLogo} alt={"trouvkach"} />
+                        <TypoGraphy variant={"title"} color={"inherit"}>
+                            {"TrouvKash"}
+                        </TypoGraphy>
+                    </Toolbar>
+                    <NavBar />
+                </AppBar>
+            </div>
+        );
+    }
+}
+export default App;
 
-import HelloWorld from "./components/hello";
-
-ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
